a3db268aa0b1fa06c7f77c049f50de407e1a4f5e
Mike Dietz <mikedietz@hotmail.com>
2019-03-13 22:19:30 +0100

    layout changes documentation.html

--- index.js
+++ index.js
@@ -91,10 +91,10 @@ app.get('/users/fav-movies/:id', (req,res) => {
     return user.id === parseInt(req.params.id);
   });
   if (findCorrectUser) {
-    usersFavMovies = findCorrectUser.favoriteMovies;
+    let usersFavMovies = findCorrectUser.favoriteMovies;
     res.json(usersFavMovies);
   }
-})
+});
 
 // new user registration
 app.post('/users', (req, res) => {
@@ -130,20 +130,23 @@ app.put('/users/fav-movies/:id/:movie', (req, res) => {
   });
   if (correctUser) {
     correctUser.favoriteMovies.push(req.params.movie);
-    res.status(201).send(req.params.movie + " was added.")
+    res.status(201).send(req.params.movie + " was added.");
   } else {
     res.status(404).send("So sorry, but the favorite movies couldn't get updated.");
   }
-})
+});
 
 // delete a user from the user list
 app.delete('/users/:name', (req, res) => {
-  let user = Users.find((user) => { return user.name === req.params.name });
+  let user = Users.find((user) => {
+    return user.name === req.params.name;
+  });
   if (user) {
     let newUsers = Users.filter(function(obj) {
-      return obj.name !== req.params.name });
+      return obj.name !== req.params.name;
+    });
       Users = newUsers;
-      res.status(201).send('User ' + req.params.name + ' was deleted.')
+      res.status(201).send('User ' + req.params.name + ' was deleted.');
   }
   else {
     res.status(404).send('Did not work, keep trying.');
@@ -156,20 +159,20 @@ app.delete('/users/fav-movies/:id/:movie', (req,res) => {
     return user.id === parseInt(req.params.id);
   });
   if (correctUser) {
-    console.log("Before removing existing movie", correctUser.favoriteMovies)
-    favMovies = correctUser.favoriteMovies
-    getIndex = favMovies.indexOf(req.params.movie)
+    console.log("Before removing existing movie", correctUser.favoriteMovies);
+    let favMovies = correctUser.favoriteMovies;
+    let getIndex = favMovies.indexOf(req.params.movie);
     // If the movie is included (-1 means, not part of our array. An array starts with position 0)
     if (getIndex !== -1) {
     // More information: https://medium.freecodecamp.org/lets-explore-slice-splice-spread-syntax-in-javascript-e242a6f21e60
       favMovies.splice(getIndex, 1);
     }
-    console.log("After removing existing movie", correctUser.favoriteMovies)
-    res.status(201).send(req.params.movie + " was deleted.")
+    console.log("After removing existing movie", correctUser.favoriteMovies);
+    res.status(201).send(req.params.movie + " was deleted.");
   } else {
     res.status(404).send("Sorry, couldn't delete the movie.");
   }
-})
+});
 
 //get all users
 app.get('/users', (req, res) => {
@@ -196,12 +199,11 @@ let Movies = [
 title: 'War Room',
 description: 'The family-friendly movie explores the transformational role prayer plays in the lives of the Jordan family. Tony and Elizabeth Jordan, a middle-class couple who seemingly have it all – great jobs, a beautiful daughter, their dream home. But appearances can be deceiving. In reality, the Jordan\'s marriage has become a war zone and their daughter is collateral damage. With the help of Miss Clara, an older, wiser woman, Elizabeth discovers she can start fighting for her family instead of against them. Through a newly energized faith, Elizabeth and Tony\'s real enemy doesn\'t have a prayer.',
 genres: ['drama', 'romance'],
-director:
-{
-name: 'Alex Kendrick',
-bio: 'former associate pastor at Sherwood Baptist Church, in Albany, Georgia, founded Sherwood Pictures, which produced Flywheel (2003), Facing the Giants (2006), Fireproof (2008), and Courageous (2011). Co-author of several best-selling books, including The Love Dare. Author of New York Times Best Seller The Resolution for Men and The Battle Plan for Prayer. Again with his brother, also appeared on the New York Times Best Seller list. In 2013, he founded Kendrick Brothers Productions with his brothers, Stephen and Shannon.',
-yearOfBirth: 1970,
-yearOfDeath: '',
+director: {
+  name: 'Alex Kendrick',
+  bio: 'former associate pastor at Sherwood Baptist Church, in Albany, Georgia, founded Sherwood Pictures, which produced Flywheel (2003), Facing the Giants (2006), Fireproof (2008), and Courageous (2011). Co-author of several best-selling books, including The Love Dare. Author of New York Times Best Seller The Resolution for Men and The Battle Plan for Prayer. Again with his brother, also appeared on the New York Times Best Seller list. In 2013, he founded Kendrick Brothers Productions with his brothers, Stephen and Shannon.',
+  yearOfBirth: 1970,
+  yearOfDeath: '',
 },
 imageUrl: 'img/warRoom.jpg'
 },
@@ -209,12 +211,11 @@ imageUrl: 'img/warRoom.jpg'
 title: 'Facing the Giants',
 description: 'A losing coach with an underdog football team faces their giants of fear and failure on and off the field to surprising results.',
 genres: ['drama'],
-director:
-{
-name: 'Alex Kendrick',
-bio: 'former associate pastor at Sherwood Baptist Church, in Albany, Georgia, founded Sherwood Pictures, which produced Flywheel (2003), Facing the Giants (2006), Fireproof (2008), and Courageous (2011). Co-author of several best-selling books, including The Love Dare. Author of New York Times Best Seller The Resolution for Men and The Battle Plan for Prayer. again with his brother, also appeared on the New York Times Best Seller list. In 2013, he founded Kendrick Brothers Productions with his brothers, Stephen and Shannon.',
-yearOfBirth: 1970,
-yearOfDeath:'',
+director: {
+  name: 'Alex Kendrick',
+  bio: 'former associate pastor at Sherwood Baptist Church, in Albany, Georgia, founded Sherwood Pictures, which produced Flywheel (2003), Facing the Giants (2006), Fireproof (2008), and Courageous (2011). Co-author of several best-selling books, including The Love Dare. Author of New York Times Best Seller The Resolution for Men and The Battle Plan for Prayer. again with his brother, also appeared on the New York Times Best Seller list. In 2013, he founded Kendrick Brothers Productions with his brothers, Stephen and Shannon.',
+  yearOfBirth: 1970,
+  yearOfDeath:'',
 },
 imageUrl: 'img/facingTheGiants.jpg'
 },
@@ -222,12 +223,11 @@ imageUrl: 'img/facingTheGiants.jpg'
 title: 'Corageous',
 description: 'As law enforcement officers, Adam Mitchell, Nathan Hayes, and their partners are confident and focused. They willingly stand up to the worst the streets have to offer. Yet at the end of the day, they face a challenge that none of them are truly prepared to tackle: fatherhood. They know that God desires to turn the hearts of fathers to their children, but their children are beginning to drift further and further away from them. When tragedy hits home, these men are left wrestling with their hopes, their fears, their faith, and their fathering. Can a new found urgency help these dads draw closer to God ... and to their children? COURAGEOUS is the fourth release of Sherwood Pictures, the movie making ministry of Sherwood Church in Albany, Georgia.',
 genres: ['drama'],
-director:
-{
-name: 'Alex Kendrick',
-bio: 'former associate pastor at Sherwood Baptist Church, in Albany, Georgia, founded Sherwood Pictures, which produced Flywheel (2003), Facing the Giants (2006), Fireproof (2008), and Courageous (2011). Co-author of several best-selling books, including The Love Dare. Author of New York Times Best Seller The Resolution for Men and The Battle Plan for Prayer. again with his brother, also appeared on the New York Times Best Seller list. In 2013, he founded Kendrick Brothers Productions with his brothers, Stephen and Shannon.',
-yearOfBirth: 1970,
-yearOfDeath:'',
+director: {
+  name: 'Alex Kendrick',
+  bio: 'former associate pastor at Sherwood Baptist Church, in Albany, Georgia, founded Sherwood Pictures, which produced Flywheel (2003), Facing the Giants (2006), Fireproof (2008), and Courageous (2011). Co-author of several best-selling books, including The Love Dare. Author of New York Times Best Seller The Resolution for Men and The Battle Plan for Prayer. again with his brother, also appeared on the New York Times Best Seller list. In 2013, he founded Kendrick Brothers Productions with his brothers, Stephen and Shannon.',
+  yearOfBirth: 1970,
+  yearOfDeath:'',
 },
 imageUrl: 'img/courageous.jpg'
 },
@@ -236,10 +236,10 @@ title: 'Paul, Apostle of Christ',
 description: 'The story covers Paul going from the most infamous persecutor of Christians to Jesus Christ\'s most influential apostle.',
 genres: ['adventure', 'biography', 'drama'],
 director: {
-name: 'Andrew Hyatt',
-bio: 'Andrew Hyatt is the award-winning writer/director of "Paul, Apostle of Christ", "Full of Grace", "The Last Light", and "The Frozen". Hyatt\'s passion is in the creation of content that is deeply personal, seeking to tell stories that delve deeper into the drama of the human condition. Exploring good and evil, truth and beauty.',
-yearOfBirth: 1982,
-yearOfDeath:'',
+  name: 'Andrew Hyatt',
+  bio: 'Andrew Hyatt is the award-winning writer/director of "Paul, Apostle of Christ", "Full of Grace", "The Last Light", and "The Frozen". Hyatt\'s passion is in the creation of content that is deeply personal, seeking to tell stories that delve deeper into the drama of the human condition. Exploring good and evil, truth and beauty.',
+  yearOfBirth: 1982,
+  yearOfDeath:'',
 },
 imageUrl: 'img/paulTheApostleOfChrist.jpg'
 },
@@ -248,10 +248,10 @@ title: 'Full of Grace',
 description: 'Follows Mary of Nazareth in her last earthly days as she helps the fractious early Church regain their original encounter with The Lord.',
 genres: ['drama', 'history'],
 director: {
-name: 'Andrew Hyatt',
-bio: 'Andrew Hyatt is the award-winning writer/director of "Paul, Apostle of Christ", "Full of Grace", "The Last Light", and "The Frozen". Hyatt\'s passion is in the creation of content that is deeply personal, seeking to tell stories that delve deeper into the drama of the human condition. Exploring good and evil, truth and beauty.' ,
-yearOfBirth: 1982,
-yearOfDeath:'',
+  name: 'Andrew Hyatt',
+  bio: 'Andrew Hyatt is the award-winning writer/director of "Paul, Apostle of Christ", "Full of Grace", "The Last Light", and "The Frozen". Hyatt\'s passion is in the creation of content that is deeply personal, seeking to tell stories that delve deeper into the drama of the human condition. Exploring good and evil, truth and beauty.' ,
+  yearOfBirth: 1982,
+  yearOfDeath:'',
 },
 imageUrl: 'img/fullOfGrace'
 },
@@ -260,10 +260,10 @@ title: 'The Passion of the Christ',
 description: 'Depicts the final twelve hours in the life of Jesus of Nazareth, on the day of his crucifixion in Jerusalem.',
 genres: ['drama'],
 director: {
-name: 'Mel Gibson',
-bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
-yearOfBirth: 1956,
-yearOfDeath:'',
+  name: 'Mel Gibson',
+  bio: 'Mel Columcille Gerard Gibson was born January 3, 1956 in Peekskill, New York, USA, as the sixth of eleven children of Hutton Gibson, a railroad brakeman, and Anne Patricia (Reilly) Gibson (who died in December of 1990). His mother was Irish, from County Longford, while his American-born father is of mostly Irish descent.',
+  yearOfBirth: 1956,
+  yearOfDeath:'',
 },
 imageUrl: 'img/thePassionOfChrist.jpg'
 },
@@ -272,10 +272,10 @@ title: 'Bird',
 description: 'The troubled life and career of the jazz musician, Charlie "Bird" Parker.',
 genres: ['biography', 'drama', 'music'],
 director: {
-name: 'Clint Eastwood',
-bio: 'Clint Eastwood was born May 31, 1930 in San Francisco, the son of Clinton Eastwood Sr., a manufacturing executive for Georgia-Pacific Corporation, and Ruth Wood, a housewife turned IBM operator. He had a comfortable, middle-class upbringing in nearby Piedmont.',
-yearOfBirth: 1930,
-yearOfDeath:'',
+  name: 'Clint Eastwood',
+  bio: 'Clint Eastwood was born May 31, 1930 in San Francisco, the son of Clinton Eastwood Sr., a manufacturing executive for Georgia-Pacific Corporation, and Ruth Wood, a housewife turned IBM operator. He had a comfortable, middle-class upbringing in nearby Piedmont.',
+  yearOfBirth: 1930,
+  yearOfDeath:'',
 },
 imageUrl: 'img/bird.jpg'
 },
@@ -284,10 +284,10 @@ title: 'Round Midnight',
 description: 'A troubled, but talented musician flees the US to escape his problems, finding refuge and support in Paris.',
 genres: ['drama', 'music'],
 director: {
-name: 'Bertrand Tavernier',
-bio: 'Bertrand Tavernier was a law student that preferred write film criticisms. He also wrote a few books about American movies. Then his first film won a few awards in France and abroad and established his reputation.',
-yearOfBirth: 1941,
-yearOfDeath:'',
+  name: 'Bertrand Tavernier',
+  bio: 'Bertrand Tavernier was a law student that preferred write film criticisms. He also wrote a few books about American movies. Then his first film won a few awards in France and abroad and established his reputation.',
+  yearOfBirth: 1941,
+  yearOfDeath:'',
 },
 imageUrl: 'img/roundMidnight.jpg'
 },
@@ -296,14 +296,14 @@ title: 'Chasing Trane: The John Coltrane Documentary',
 description: 'The film explores the global power and impact of the music of John Coltrane and reveals the passions, experiences and forces that shaped his life and revolutionary sounds.',
 genres: ['documentary', 'biography', 'music'],
 director: {
-name: 'John Scheinfeld',
-bio: 'From pop culture to politics, sports to world religions, Venice and Toronto film festivals to PBS, Emmy®, Grammy® and Writers Guild Award nominee John Scheinfeld is a critically-acclaimed documentary filmmaker with a broad range of subjects and productions to his credit.',
-yearOfBirth:'' ,
-yearOfDeath:'',
+  name: 'John Scheinfeld',
+  bio: 'From pop culture to politics, sports to world religions, Venice and Toronto film festivals to PBS, Emmy®, Grammy® and Writers Guild Award nominee John Scheinfeld is a critically-acclaimed documentary filmmaker with a broad range of subjects and productions to his credit.',
+  yearOfBirth:'' ,
+  yearOfDeath:'',
 },
 imageUrl: 'img/Coltrane.jpg'
 }
-]
+];
 
 let Users = [
 {
@@ -333,8 +333,8 @@ email: 'quant@quint.com',
 dob: '22.11.1918',
 favoriteMovies : ['Bird']
 }
-]
+];
 
 app.listen(3000, () => {
   console.log('This app is listening on port 3000.');
-})
+});
