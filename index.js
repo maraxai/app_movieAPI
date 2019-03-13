6d13f079edb648cb49c8f9536aca896ba5cf0114
maraxai <mikedietz@hotmail.com>
2019-03-13 11:04:06 +0100

    consolidated js file

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ app.get('/movies/:title/genres', (req, res) => {
 
   if (movie) {
     let genresArray = Object(movie.genres);
+    console.log(genresArray);
     res.status(201).send(genresArray);
   }
   else {
@@ -84,6 +85,17 @@ app.get('/movies/directors/:name', (req, res) => {
   }
 });
 
+// get a user's list of favorite movies by id
+app.get('/users/fav-movies/:id', (req,res) => {
+  let findCorrectUser = Users.find((user) => {
+    return user.id === parseInt(req.params.id);
+  });
+  if (findCorrectUser) {
+    usersFavMovies = findCorrectUser.favoriteMovies;
+    res.json(usersFavMovies);
+  }
+})
+
 // new user registration
 app.post('/users', (req, res) => {
   let newUser = req.body;
@@ -111,27 +123,18 @@ app.put('/users/:name/:username/:password/:email', (req, res) => {
   }
 });
 
-// add movie to list of favoriteMovies
-app.put('/users/:user/favoriteMovies/:title', (req, res) => {
-  let user = Users.find((user) => {
-    return user.name === req.params.name;
+// add movie to the list of favoriteMovies of a user identified by id
+app.put('/users/fav-movies/:id/:movie', (req, res) => {
+  let correctUser = Users.find((user) => {
+    return user.id === parseInt(req.params.id);
   });
-  if (user) {
-    let title = Movies.find((title) => {
-      return Movies.title === req.params.title;
-
-      if (title) {
-        let newFavMovie = user.favoriteMovies;
-        newFavMovie.push(req.params.title);
-        return newFavMovie;
-        res.status(201).send('The movie ' +  req.params.title + ' has been added to your list of favorite movies.');
-      }
-    });
+  if (correctUser) {
+    correctUser.favoriteMovies.push(req.params.movie);
+    res.status(201).send(req.params.movie + " was added.")
+  } else {
+    res.status(404).send("So sorry, but the favorite movies couldn't get updated.");
   }
-  else {
-    res.status(404).send('Doesn\'t work!')
-  }
-});
+})
 
 // delete a user from the user list
 app.delete('/users/:name', (req, res) => {
@@ -148,214 +151,17 @@ app.delete('/users/:name', (req, res) => {
 });
 
 // remove movie in list of favoriteMovies
-app.delete('/users/:user/favoriteMovies/:title', (req, res) => {
-  let user = Users.find((user) => {
-    return user.name === req.params.name;
-  });
-  if (user) {
-    let updMovie = Users.favoriteMovies.filter(function(obj) {
-      return obj.favoriteMovies !== req.params.title });
-      Users.favoriteMovies = updMovie;
-      res.status(201).send('Delete request will remove the movie ' + req.params.title + ' from the list of favorite movies.');
-  }
-  else {
-    res.status(404).send('Keep trying! Deletion of movie from the favorites has not been successful!');
-  }
-});
-
-///////////////////////////////// SVENJA'S CODE
-// The favorite Movies issue :)
-
-let user = {
-    name: 'Mike',
-    id: 1,
-    username: 'mikedietz',
-    password: 'test8test',
-    email: 'here@there.com',
-    dob: '11.11.1919',
-    favoriteMovies : [ 'War Room', 'Bird', 'Round Midnight' ]
-}
-
-// What do we want to do? We want to add a string to our favoriteMovies Array.
-// So, first of all, we need a title. Let's declare it:
-
-let newTitle = 'Rotkaeppchen'
-console.log(newTitle + ' (line:183)');
-
-// Now, we need a JavaScript method to push something into an array. Sometimes
-// it's quite lovely as the method has a very semantic name. In this case, it's push().
-// More information here: https://appdividend.com/2018/10/08/javascript-array-push-example-array-prototype-tutorial/
-
-let newTitleForFavoriteMoviews = user.favoriteMovies.push(newTitle + ' und der Wolf');
-
-// To see if we have been successfull, log the favoriteMovies array:
-console.log(user.favoriteMovies + ' (line:192)')
-
-// The output will be: [ 'War Room', 'Bird', 'Round Midnight', 'Rotkaeppchen' ]
-
-// As you can see, for one user it's quite easy. But, we have more than one user:
-
-let group = [{
-  name: 'Mike',
-  id: 1,
-  username: 'mikedietz',
-  password: 'test8test',
-  email: 'here@there.com',
-  dob: '11.11.1919',
-  favoriteMovies : [ 'War Room', 'Bird', 'Round Midnight' ]
-},
-{
-  name: 'Quantino',
-  id: 3,
-  username: 'quantinto',
-  password: 'quantanto',
-  email: 'quant@quint.com',
-  dob: '22.11.1918',
-  favoriteMovies : ['Bird']
-}
-]
-
-// So, this time, we first have to find the correct user inside our group variable. For
-// simplicity we'll use the id as identification. As the name (find) suggests, the
-// find() method is an option: More information: https://appdividend.com/2018/12/17/javascript-array-find-example-array-prototype-find-tutorial/
-
-let findCorrectUser = group.find(user => user.id === 1);
-
-
-// Again, let's check the if we found 'Mike':
-console.log(findCorrectUser);
-
-// The output is:
-// { name: 'Mike',
-//   id: 1,
-//   username: 'mikedietz',
-//   password: 'test8test',
-//   email: 'here@there.com',
-//   dob: '11.11.1919',
-//   favoriteMovies: [ 'War Room', 'Bird', 'Round Midnight' ] }
-
-// But we want more. We want Mikes' favorite movies. And it's quite simple:
-console.log(findCorrectUser.favoriteMovies)
-
-// output: [ 'War Room', 'Bird', 'Round Midnight' ]
-
-// Awesome. Now, we have to combine both methods. First, we find the correct user and
-// after that we want to add a new favorite movie:
-
-let newFavoriteMoviewForMike = findCorrectUser.favoriteMovies.push(newTitle)
-
-// Again, log to be safe:
-console.log(findCorrectUser)
-
-// output:
-// { name: 'Mike',
-//   id: 1,
-//   username: 'mikedietz',
-//   password: 'test8test',
-//   email: 'here@there.com',
-//   dob: '11.11.1919',
-//   favoriteMovies: [ 'War Room', 'Bird', 'Round Midnight', 'Rotkaeppchen' ] }
-
-// So, what did we do? We found a user and add new properties to this user.
-// Let's try to use this knowledge for our express application.
-
-// We start with finding a single user by its id:
-
-app.get('/users/:id', (req, res) => {
-  res.json(Users.find((user) => {
-    // First we loop through our Users collection, to find the user with id: 1
-    // To see, what kind of request params we get, ALWAYS log them. :)
-    console.log(req.params + ' line 268')
-    // the output is: { id: '1' }
-    // Now, we want to RETURN the user with this id. The users id has a type
-    // of an Integer, but the request is a string. So, we need to find a method
-    // which converts a string into an integer. Very semantic again: parseInt().
-    // More information: https://www.w3schools.com/jsref/jsref_parseint.asp
-    return user.id === parseInt(req.params.id);
-    // the output is:
-    // {
-    //   "name": "Mike",
-    //   "id": 1,
-    //   "username": "mikedietz",
-    //   "password": "test8test",
-    //   "email": "here@there.com",
-    //   "dob": "11.11.1919",
-    //   "favoriteMovies": [
-    //       "War Room",
-    //       "Bird",
-    //       "Round Midnight"
-    //   ]
-    // }
-  }));
-});
-
-// Okay, the user is here, but we want his favorite movies.
-// Have a look: don't use capital letters in your URL, please. :)
-// More information: https://www.quora.com/Should-URLs-ever-have-capital-letters
-app.get('/users/fav-movies/:id', (req,res) => {
-  // first we find the correctUser and set it to an variable
-  let findCorrectUser = Users.find((user) => {
-    return user.id === parseInt(req.params.id);
-  });
-  if (findCorrectUser) {
-    // if we find the user, let's find the favorite movies.
-    usersFavMovies = findCorrectUser.favoriteMovies;
-    // And finally, output the favorite movies. Just
-    // like the example above.
-    res.json(usersFavMovies);
-    // The output is:
-    // [
-    //   "War Room",
-    //   "Bird",
-    //   "Round Midnight"
-    // ]
-  }
-})
-
-// Next step, adding a new title to the favorite movies:
-app.put('/users/fav-movies/:id/:movie', (req, res) => {
-  // again, first we need the correct user
-  let correctUser = Users.find((user) => {
-    return user.id === parseInt(req.params.id);
-  });
-  if (correctUser) {
-    // after that, let's check the status of the favoriteMovies array
-    console.log("Before adding new movie", correctUser.favoriteMovies)
-    // we use the method from earlier, push()
-    correctUser.favoriteMovies.push(req.params.movie);
-    // and check again, if the new movie was added
-    console.log("After adding new movie", correctUser.favoriteMovies)
-    // we send a lovely note, which movie was added. You could add the name of the user or
-    // whatever message you want. Play around with the parameters to get a feeling on how
-    // to access the different parameters.
-    res.status(201).send(req.params.movie + " was added.")
-  } else {
-    res.status(404).send("So sorry, but the favorite movies couldn't get updated.");
-  }
-})
-
-// Finally, we want to remove an item from an array. There are different options,
-// you could use filter(), but this would return a copy of the array. It wouldn't
-// really remove the item. Some methods manipulate the array, others not.
-// More information: https://lorenstewart.me/2017/01/22/javascript-array-methods-mutating-vs-non-mutating/
-// Okay, we'll use the splice() method. The first option is for array, where you want to
-// remove the first match. Afterwards, we'll use a loop to remove all items in case your
-// array has double items.
 app.delete('/users/fav-movies/:id/:movie', (req,res) => {
   let correctUser = Users.find((user) => {
     return user.id === parseInt(req.params.id);
   });
   if (correctUser) {
     console.log("Before removing existing movie", correctUser.favoriteMovies)
-    // for readability we declare a variable for our favorite movies
     favMovies = correctUser.favoriteMovies
-    // now, we want to know where movie is located in our array
-    // More information: https://www.w3schools.com/jsref/jsref_indexof.asp
     getIndex = favMovies.indexOf(req.params.movie)
     // If the movie is included (-1 means, not part of our array. An array starts with position 0)
     if (getIndex !== -1) {
-      // we manipulate the original array and remove the movie
-      // More information: https://medium.freecodecamp.org/lets-explore-slice-splice-spread-syntax-in-javascript-e242a6f21e60
+    // More information: https://medium.freecodecamp.org/lets-explore-slice-splice-spread-syntax-in-javascript-e242a6f21e60
       favMovies.splice(getIndex, 1);
     }
     console.log("After removing existing movie", correctUser.favoriteMovies)
@@ -364,34 +170,6 @@ app.delete('/users/fav-movies/:id/:movie', (req,res) => {
     res.status(404).send("Sorry, couldn't delete the movie.");
   }
 })
-
-// And now, the version if we have more than one "Bird" for example:
-app.delete('/users/fav-movies/:id/:movie', (req,res) => {
-  let correctUser = Users.find((user) => {
-    return user.id === parseInt(req.params.id);
-  });
-  if (correctUser) {
-    console.log("Before removing existing movie", correctUser.favoriteMovies)
-    favMovies = correctUser.favoriteMovies
-    // the main difference is, that we don't use indexOf(), but a
-    // classic for loop which iterates over each movie.
-    for (var i=favMovies.length-1; i>=0; i--) {
-      if (favMovies[i] === req.params.movie) {
-        favMovies.splice(i, 1);
-      }
-    }
-    console.log("After removing existing movie", correctUser.favoriteMovies)
-    res.status(201).send(req.params.movie + " was deleted.")
-  } else {
-    res.status(404).send("Sorry, couldn't delete the movie.");
-  }
-})
-
-// Remember, you can log everything, so use this tool whenever you write code. When you
-// know your parameters and you know what your result should look like, coding
-// gets much easier. :)
-// Happy coding!
-//////////////////////////////////
 
 //get all users
 app.get('/users', (req, res) => {
@@ -402,6 +180,14 @@ app.get('/users', (req, res) => {
 app.get('/users/:name', (req, res) => {
   res.json(Users.find((user) => {
     return user.name === req.params.name;
+  }));
+});
+
+// get a user's data by id (name, id, username, password, email, dob)
+app.get('/users/:id', (req, res) => {
+  res.json(Users.find((user) => {
+    console.log(req.params);
+    return user.id === parseInt(req.params.id);
   }));
 });
 
